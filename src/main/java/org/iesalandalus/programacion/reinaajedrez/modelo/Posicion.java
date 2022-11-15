06630a4495285e46a9977bfd079a5b58f8351fea
BadPi88 <miguel88martin@gmail.com>
2022-11-15 14:15:24 +0100

    Creada clase copia

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,20 +1,50 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
+	
 	private int fila;
 	private char columna;
+	
+	private Posicion(int fila){
+		setFila(fila);
+		if(fila < 1 || fila > 8) {
+			throw new NullPointerException ("El movimiento fila no es valido");
+			}
+		
+		setColumna(columna);
+		if(columna < 'a'|| columna > 'h' ) {
+			throw new NullPointerException ("El movimiento en columna no es valido");
+		}
+	}
+	
+	private Posicion(char columna) {
+		setColumna(columna);
+		if(columna < 'a'|| columna > 'h' ) {
+			throw new NullPointerException ("El movimiento en columna no es valido");
+		}
+	this.columna = columna;
+	}
+	
 	public int getFila() {
 		return fila;
 	}
-	public void setFila(int fila) {
-		this.fila = fila;
-	}
+	
 	public char getColumna() {
 		return columna;
 	}
+	
 	private void setColumna(char columna) {
-		this.columna = columna;
+		if(columna < 'a'|| columna > 'h' ) {
+			throw new NullPointerException ("El movimiento no es valido");
+		}
+	this.columna = columna;
 	}
 	
-
+	private void setFila(int fila) {
+		if(fila < 1 || fila > 8) {
+			throw new NullPointerException ("El movimiento no es valido");
+		}
+		this.fila= fila;
+	}
+	
 }
