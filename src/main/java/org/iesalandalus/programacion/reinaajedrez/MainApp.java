e8c82e68ef6cb14698abec2231c0490904baee2d
BadPi88 <miguel88martin@gmail.com>
2022-11-17 09:53:14 +0100

    Creado el atributo de clase reina. Creados metodos para definir la instacia, color y movimiento

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,33 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Consola;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
 	
-	public static void main(String[] args) {
+	static Reina reina;
+	private static void crearReinaPorDefecto() {
+	 	reina = new Reina();
+	}
+	private static void crearReinaColor() {
+		reina = new Reina(Consola.elegirColor());
+	}
+	private static void mover(){
+		Consola.mostrarMenu();
+		try {
+			reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+		} catch (OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		
 	}
+	
+	public static void main(String[] args){
+		
+			
+		}
+	
 }
